b6e3608c86fa67564bb34581c24c80359d84e9f5
Tim Millard <tim@rapidpaylegal.com>
2022-12-12 01:29:48 +1100

    inital commit

---
+++ Formula/slang.rb
@@ -0,0 +1,8 @@
+class Slang < Formula
+  desc "Command line tool to help keep up with the hipsters."
+  homepage "https://github.com/timwmillard/slang"
+  license "MIT"
+  head "https://github.com/timwmillard/slang.git", branch: "master"
+
+end
+
