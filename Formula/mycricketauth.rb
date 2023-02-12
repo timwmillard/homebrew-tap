0d055b773707ea4a115c755aa5d82cc5779591a3
Tim Millard <timwmillard@gmail.com>
2023-02-13 00:31:45 +1100

    Update sha

--- Formula/mycricketauth.rb
+++ Formula/mycricketauth.rb
@@ -9,7 +9,7 @@ class Mycricketauth < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://drive.google.com/file/d/1QpzRsV8EIRe_cieMuTmqt3gJLvK_BN08/view?usp=share_link"
-      sha256 "a38326a3285741f6bf63966ea5c85d5de2eed259bec4838325ac3e24c33f4a13"
+      sha256 "696f22b2f24e4186fd5236d427097b0b0dc23c5735ca33d06ea8c00785d2a32c"
 
       def install
         bin.install "mycricketauth"
@@ -17,7 +17,7 @@ class Mycricketauth < Formula
     end
     if Hardware::CPU.arm?
       url "https://drive.google.com/file/d/1QtBpuO2mPyQH9Pu2OPhiq-O99DmTy85Y/view?usp=share_link"
-      sha256 "a6575baeecf04aea1078ff5f7d1c8865ca5ad9c76ed4b7c6b949b90ee2349f29"
+      sha256 "6425f0095ceb2c7f3c7e36da94d35b5d48ceef7bf1390e07ea5bce3714778740"
 
       def install
         bin.install "mycricketauth"
